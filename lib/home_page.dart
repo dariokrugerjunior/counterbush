af36a2cda8e04c869f110af1702c341fedb43e0c
dariokrugerjunior <krugerdario1@gmail.com>
2023-08-13 04:11:04 -0300

    Adicionando botão de tela cheia e também salvar na galeria

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,6 +3,10 @@ import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'dart:typed_data';
 import 'api/robo_flow.dart';
+import 'package:flutter/services.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+import 'package:path_provider/path_provider.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -19,6 +23,7 @@ class _HomePageState extends State<HomePage> {
   ApiService apiService = ApiService('VaFSU2CW1skuZYeidgCM');
 
   Future<void> searchApiImage(File imageFile) async {
+    showSnackBar("Pesquisando Maçãs");
     try {
       classCount = await apiService.sendImageForPrediction(imageFile);
       imageBytes = await apiService.sendImageForPredictionFile(imageFile);
@@ -26,20 +31,76 @@ class _HomePageState extends State<HomePage> {
         imageSelected = null; // Remover a imagem selecionada
       });
     } catch (e) {
-      print('Erro: $e');
+      showSnackBar('Erro: $e');
     }
+  }
+
+   Future<void> _saveImageToGallery(Uint8List bytes) async {
+    try {
+      final directory = await getTemporaryDirectory();
+      final tempFilePath = '${directory.path}/temp_image.png';
+
+      final tempFile = File(tempFilePath);
+      await tempFile.writeAsBytes(bytes);
+
+      final result = await GallerySaver.saveImage(tempFilePath, toDcim: true);
+      if (result!) {
+        showSnackBar('Imagem salva na galeria com sucesso.');
+      } else {
+        showSnackBar('Error ao salvar imagem na galeria.');
+      }
+
+      // Após salvar, você pode excluir o arquivo temporário se necessário
+      await tempFile.delete();
+    } catch (e) {
+      showSnackBar('Error: $e');
+    }
+  }
+
+
+  void openFullScreenImage(Uint8List bytes) {
+    Navigator.push(
+      context,
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(child: Image.memory(bytes)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          _saveImageToGallery(bytes);
+                        },
+                        child: const Text('Salvar na Galeria'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 
   List<Widget> buildClassCountWidgets() {
     return classCount.entries.map((entry) {
       return Card(
         child: ListTile(
-          title: Text('Maça: ${getColorApple(entry.key)}'),
+          title: Text('Maçã: ${getColorApple(entry.key)}'),
           subtitle: Text('Quantidade: ${entry.value}'),
         ),
       );
     }).toList();
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -50,14 +111,44 @@ class _HomePageState extends State<HomePage> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           if (imageBytes == null && imageSelected != null)
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Image.file(imageSelected!),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        searchApiImage(imageSelected!);
+                      },
+                      child: const Text('Pesquisar Maçãs'),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Image.file(imageSelected!),
+                ),
+              ],
             ),
           if (imageBytes != null)
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Image.memory(imageBytes!),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        openFullScreenImage(imageBytes!);
+                      },
+                      child: const Text('Ver em Tela Cheia'),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Image.memory(imageBytes!),
+                ),
+              ],
             ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -75,14 +166,6 @@ class _HomePageState extends State<HomePage> {
                 icon: const Icon(Icons.photo_camera_outlined),
               ),
             ],
-          ),
-          ElevatedButton(
-            onPressed: () {
-              if (imageSelected != null) {
-                searchApiImage(imageSelected!);
-              }
-            },
-            child: const Text('Pesquisar Maçãs'),
           ),
           Expanded(
             child: ListView(
@@ -131,6 +214,15 @@ class _HomePageState extends State<HomePage> {
     }
     return name;
   }
+
+  void showSnackBar(String message) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(message),
+      duration: const Duration(seconds: 2), // Adjust the duration as needed
+    ),
+  );
+}
 }
 
 void main() {
